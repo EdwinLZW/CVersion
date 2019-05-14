0bfe8214f533098149cc10c5a2f8fd4dd620e2e5
simonlzw <39589972+simonlzw@users.noreply.github.com>
2019-05-14 18:34:57 +0800

    Update test.py

--- ImgAnalysisSys/Model/test.py
+++ ImgAnalysisSys/Model/test.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__author__ = "Zhouwu Liu"
-__copyright__ = "Copyright 2019, PRMeasure Inc."
-__email__ = "zhouwu.liu@prmeasure.com"
+__author__ = "Simon Liu"
 
 import numpy as np
 import os
