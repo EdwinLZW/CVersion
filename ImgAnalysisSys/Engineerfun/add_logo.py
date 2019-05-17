fbddd133b5aa6b5a849c244bb8e4557fb41bcdf9
Simon <17607181126@163.com>
2019-05-17 23:09:54 +0800

    update

--- ImgAnalysisSys/Engineerfun/add_logo.py
+++ ImgAnalysisSys/Engineerfun/add_logo.py
@@ -14,8 +14,9 @@ class AddLogoToImage(object):
         if len(args) == 4:
             __bg_img = self._img_obj.acquire_image_info(bgimg)
             __logo_img = cv2.resize(self._img_obj.acquire_image_info(logoimg, 1), (args[0], args[1]))
+
             rows, clos, channel = __logo_img.shape
-            roi = __bg_img[__bg_img.shape[0] - rows:__bg_img.shape[0], __bg_img.shape[0] - clos:__bg_img.shape[0]]
+            roi = __bg_img[__bg_img.shape[0] - rows:__bg_img.shape[0], __bg_img.shape[1] - clos:__bg_img.shape[1]]
 
             logo_img_gray = cv2.cvtColor(__logo_img, cv2.COLOR_BGR2GRAY)
             ret, logo_img_mask = cv2.threshold(logo_img_gray, args[2], args[3], cv2.THRESH_BINARY)
@@ -27,7 +28,7 @@ class AddLogoToImage(object):
             dst = cv2.add(banary_roi, banaryand_logo_img)
             __bg_img[__bg_img.shape[0] - rows:__bg_img.shape[0], __bg_img.shape[0] - clos:__bg_img.shape[0]] = dst
 
-            cv2.imshow('addlogo', __bg_img)
+            cv2.imshow('addlogo', cv2.resize(__bg_img, (200,200)))
             cv2.waitKey(0)
         else:
             raise ImportError, 'Logo size imput ERROR!'
---
+++ ImgAnalysisSys/Engineerfun/color_recognition.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = "Simon Liu"
+
+import cv2
+import numpy as np
+
+
+from Model.BasicOperation import VideoBasicOperation
+
+
+class ColorRecognition(VideoBasicOperation):
+    def __init__(self):
+        super(ColorRecognition, self).__init__()
+
+    def identify_color(self, *args):
+        self.cap = self.get_capture_video(0)
+        if isinstance(self.cap, cv2.VideoCapture):
+            while self.cap.isOpened():
+                __ret,  __frame = self.cap.read()
+                hsv = cv2.cvtColor(__frame, cv2.COLOR_BGR2HSV)
+                lower_blue = np.array(args[0])
+                upper_blue = np.array(args[1])
+                cap_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+                res = cv2.bitwise_and(__frame, __frame, mask=cap_mask)
+                cv2.imshow('res', res)
+                k = cv2.waitKey(1) & 0xFF
+                if k == ord('q'):
+                    break
+            cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    core = ColorRecognition()
+    core.identify_color([110, 50, 50], [130, 255, 255])
+
+
